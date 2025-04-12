2b097ebe07361efd3be7a0c6bc21dcdcc3c499ee
JashwanthB7 <bathineni.ganesh@gmail.com>
2025-04-12 15:15:10 -0500

    changes to stars

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function createStars() {
     }
   }
   createStars();
+
+  function createStars() {
+    const container = document.querySelector("body");
+    for(let i = 0; i < 1200; i++) {
+      const star = document.createElement("div");
+      star.className = "star";
+      star.style.width = (Math.random() * 1.5 + 0.5) + "px";
+      star.style.height = star.style.width;
+      star.style.left = Math.random() * 100 + "%";
+      star.style.top = Math.random() * 100 + "%";
+      container.appendChild(star);
+    }
+  }
+  createStars();
