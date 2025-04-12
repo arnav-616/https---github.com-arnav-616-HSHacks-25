01300b3913102f379fd529ede5cbbad0d37e0b11
arnav-616 <arnavpil0725@gmail.com>
2025-04-12 12:35:10 -0500

    lots

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function createStars() {
     }
   }
   createStars();
+
+  let popup = document.getElementById("popup");
+
+  function openPopup(){
+    popup.classList.add("open-popup");
+  }
+  function closePopup(){
+    popup.classList.remove("open-popup");
+  }
